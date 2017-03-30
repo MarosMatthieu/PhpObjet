b12a5480556ad1ced47bb6a3d99d600db10c98ae
15marosm <mmaroswork@gmail.com>
2017-03-30 11:22:43 +0200

    begin indiv

---
+++ src/Domain/Individu.php
@@ -0,0 +1,45 @@
+<?php
+
+/***
+ * Class Individu
+ * @author 15marosm
+ */
+class Individu {
+	
+	private $indivNom;
+	private $indivPrenom;
+	private $indivAge;
+	private $indivShortDesc;
+	
+	
+	/**
+	 * Get & Set 
+	 */
+	public function getIndivNom() 
+	{
+		return $this->indivNom;
+	}
+	public function setIndivNom($indivNom)
+	{
+		return $this->indivNom = $indivNom ;
+	}
+	
+	public function getIndivPrenom()
+	{
+		return $this->indivPrenom;
+	}
+	public function setIndivNom($indivPrenom)
+	{
+		return $this->indivPrenom = $indivPrenom ;
+	}
+	
+
+}
+
+
+
+
+
+constructeur
+
+show indiv
