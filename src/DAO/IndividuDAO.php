f9e8b9317aa019ee11457bf155183438255b9d49
15marosm <mmaroswork@gmail.com>
2017-04-03 14:18:08 +0200

    changement SQl

--- src/DAO/IndividuDAO.php
+++ src/DAO/IndividuDAO.php
@@ -10,21 +10,21 @@ class IndividuDAO {
 	public function get1($id) {
 		$id = ( int ) $id;
 		
-		$q = $this->_db->query ( 'SELECT id, titre, date, texte_nouvelle FROM news WHERE id = ' . $id );
+		$q = $this->_db->query ( 'SELECT id, nom, prenom, age ,shortDesc FROM INDIVIDU WHERE id = ' . $id );
 		$donnees = $q->fetch ( PDO::FETCH_ASSOC );
 		
 		return new News ( $donnees ['id'], $donnees ['titre'], $donnees ['date'], $donnees ['texte_nouvelle'] );
 	}
 	public function getList() {
-		$lstNews = [ ];
+		$lstIndiv = [ ];
 		
-		$q = $this->_db->query ( 'SELECT id, titre, date, texte_nouvelle FROM news ORDER BY date DESC' );
+		$q = $this->_db->query ( 'SELECT id, titre, date, texte_nouvelle FROM news ORDER BY id ASC' );
 		
 		while ( $donnees = $q->fetch ( PDO::FETCH_ASSOC ) ) {
-			$lstNews [] = new News ( $donnees ['id'], $donnees ['titre'], $donnees ['date'], $donnees ['texte_nouvelle'] );
+			$lstIndiv [] = new News ( $donnees ['id'], $donnees ['titre'], $donnees ['date'], $donnees ['texte_nouvelle'] );
 		}
 		
-		return $lstNews;
+		return $lstIndiv;
 	}
 }
 
