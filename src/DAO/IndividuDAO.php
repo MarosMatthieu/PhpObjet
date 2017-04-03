a72a9e594b4d786fed5511332992779a6408e985
15marosm <mmaroswork@gmail.com>
2017-04-03 14:47:35 +0200

    Creation des methodes

--- src/DAO/IndividuDAO.php
+++ src/DAO/IndividuDAO.php
@@ -1,31 +1,57 @@
 <?php
 class IndividuDAO {
 	
+	private $bdd;
+	
+	
 	//CRUD de INDIVIDU + LIST individu
 	
-	private $bdd;
+	
 	function __construct($bdd) {
 		$this->bdd = $bdd;
 	}
-	public function get1($id) {
+	
+	public function getSingleIndiv($id) {
 		$id = ( int ) $id;
 		
 		$q = $this->_db->query ( 'SELECT id, nom, prenom, age ,shortDesc FROM INDIVIDU WHERE id = ' . $id );
 		$donnees = $q->fetch ( PDO::FETCH_ASSOC );
 		
-		return new News ( $donnees ['id'], $donnees ['titre'], $donnees ['date'], $donnees ['texte_nouvelle'] );
+		return new News ( $donnees ['id'], $donnees ['nom'], $donnees ['prenom'], $donnees ['age'], $donnees ['shortDesc'] );
 	}
-	public function getList() {
+		
+	public function getListIndiv() {
 		$lstIndiv = [ ];
 		
-		$q = $this->_db->query ( 'SELECT id, titre, date, texte_nouvelle FROM news ORDER BY id ASC' );
+		$q = $this->_db->query ( 'SELECT id, nom, prenom, age ,shortDesc FROM INDIVIDU WHERE id ASC' );
 		
 		while ( $donnees = $q->fetch ( PDO::FETCH_ASSOC ) ) {
-			$lstIndiv [] = new News ( $donnees ['id'], $donnees ['titre'], $donnees ['date'], $donnees ['texte_nouvelle'] );
+			$lstIndiv [] = new News ( $donnees ['id'], $donnees ['nom'], $donnees ['prenom'], $donnees ['age'], $donnees ['shortDesc'] );
 		}
 		
 		return $lstIndiv;
 	}
+
+	public function addIndiv($id) {
+//		$id = ( int ) $id;
+//	
+//		$q = $this->_db->query ( 'SELECT id, nom, prenom, age ,shortDesc FROM INDIVIDU WHERE id = ' . $id );
+//		$donnees = $q->fetch ( PDO::FETCH_ASSOC );
+	
+//		return new News ( $donnees ['id'], $donnees ['nom'], $donnees ['prenom'], $donnees ['age'], $donnees ['shortDesc'] );
+	}
+
+	public function DelIndiv($id) {
+//		$id = ( int ) $id;
+	
+//		$q = $this->_db->query ( 'SELECT id, nom, prenom, age ,shortDesc FROM INDIVIDU WHERE id = ' . $id );
+//		$donnees = $q->fetch ( PDO::FETCH_ASSOC );
+	
+//		return new News ( $donnees ['id'], $donnees ['nom'], $donnees ['prenom'], $donnees ['age'], $donnees ['shortDesc'] );
+	}
+
 }
+
+
 
 ?>
